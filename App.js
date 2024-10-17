9a63f10ceb2f604045fb8f6b9856e3b876002008
Anup Singh <anup.10745342@ltimindtree.com>
2024-10-17 17:40:40 +0530

    Episode 3

--- App.js
+++ App.js
@@ -1,30 +1,22 @@
         
-        /** 
-         *  <div id="parent">
-         *   <div id="child">
-         *   <h1>I am an h1 tag</h1>
-         *   <h2> I am in h2 Tag </h2>
-         *   </div>
-         *  </div>
-         * 
-         */
+        import React from 'react';
+        import ReactDOM from 'react-dom/client';
         
-         const parent = React.createElement("div",{id:"parent"},[React.createElement("div",{id:"child1"},[
-            React.createElement("h1",{},"I am an h1 tag"),
-            React.createElement("h2",{},"I am an h2 tag"),
-         ]),
-            React.createElement("div",{id:"child2"},[
-            React.createElement("h1",{},"I am an h1 tag"),
-            React.createElement("h2",{},"I am an h2 tag"),
-            ]),
-        ]);
+        const Title =()=> (
+            <h1 className="head" tabIndex="5">
+            Namaste React using JSX 📈📈
+            </h1>
+                );
+      
+        const HeadingComponent = () => (
+            <div id ="container">
+                  {Title()}
+                  <Title />
+                  <Title></Title>
+                  <h1 className="heading">Namaste React Functional Component </h1>
+            </div>
+           );
 
-        console.log(parent);
-        const root= ReactDOM.createRoot(document.getElementById("root"));
-        root.render(parent);
-        
+       const root = ReactDOM.createRoot(document.getElementById("root"));
+       root.render(<HeadingComponent />);
 
-      //  const heading = React.createElement("h1", {id:"heading"}, "Hello World from React!");
-      //  console.log(heading);
-      //  const root = ReactDOM.createRoot(document.getElementById("root"));
-      //  root.render(heading);
---
+++ README.md
@@ -0,0 +1,19 @@
+# Namaste React
+
+# parcel
+- Dev
+- Build Server
+- HMR = Hot Build Replacement
+- File Watching Algorithm written by C++
+- Image Optimization
+- Minification
+- Bundling
+- Compressing 
+- Consistent Hashing 
+- Code Splitting 
+- Differential Bundling - support older browsers
+- Diagnostic
+- Error Handling
+- HTTPS
+- Tree Shaking - remove unused code 
+- Different dev and prod bundles 
